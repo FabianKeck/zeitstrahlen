d8cdf34960cb5c1cc62b41ac98770961691494ce
Fabian Keck <fabian.keck@posteo.de>
2020-11-13 22:08:20 +0100

    Init backend server

---
+++ backend/src/main/java/de/neuefische/zeitstrahlenserver/ZeitstrahlenServerApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.zeitstrahlenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeitstrahlenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeitstrahlenServerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/neuefische/zeitstrahlenserver/ZeitstrahlenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.zeitstrahlenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeitstrahlenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
